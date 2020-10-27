223744a9f920381f985a52ac5b54ab1353dde200
natalie_davies <natalie.d.davies@outlook.com>
2020-10-27 18:08:19 +0800

    Updated files

--- d3_data_journalism/assets/js/app.js
+++ d3_data_journalism/assets/js/app.js
@@ -8,9 +8,7 @@ d3.csv("assets/data/data.csv").then(data => {
 // _______________________________________________________________CHART SETUP_______________________________________________________________________//
 var svgWidth = 1000;
 var svgHeight = 700;
-
 var margin = {top: 20, right: 40, bottom: 60, left: 50};
-
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
@@ -30,55 +28,23 @@ const yAxis = d3.axisLeft(yScale);
 chartGroup.append("g").attr("transform", `translate(0, ${height})`).call(xAxis);
   chartGroup.append("g").call(yAxis);
 
-//============Generate scatter plot=========
-chartGroup.selectAll("circle")
-.data(data)
-.enter()
-.append("circle")
+// _______________________________________________________________MAKE SCATTER & ADD TEXT_______________________________________________________________________//
+chartGroup.selectAll("circle").data(data).enter().append("circle").classed("stateCircle", true)
+.style("fill", "#e38690")
 .attr("cx", d=>xScale(d.income))
 .attr("cy", d=>yScale(d.smokes))
-.attr("r", "10")
+.attr("r", "15")
 .attr("stroke-width", "1")
-.classed("stateCircle", true)
-.attr("opacity", 0.75);
+.attr("opacity", 0.50);
 
-//============add texts to each datapoint=========
-chartGroup.append("g")
-  .selectAll('text')
-  .data(data)
-  .enter()
-  .append("text")
-  .text(d=>d.abbr)
+chartGroup.append("g").selectAll('text').data(data).enter().append("text").text(d=>d.abbr)
   .attr("x",d=>xScale(d.income))
   .attr("y",d=>yScale(d.smokes))
-  .classed(".stateText", true)
-  .attr("font-family", "sans-serif")
   .attr("text-anchor", "middle")
-  .attr("fill", "white")
+  .attr("fill", "black")
   .attr("font-size", "10px")
-  .style("font-weight", "bold")
   .attr("alignment-baseline", "central");
   
-  //============add axes titles=========
-  chartGroup.append("text")
-        .attr("transform", `translate(${width / 2}, ${height + margin.top + 13})`)
-        .attr("text-anchor", "middle")
-        .attr("font-size", "16px")
-        .attr("fill", "black")
-        .style("font-weight", "bold")
-        .text("income");
-
-        chartGroup.append("text")
-        .attr("y", 0 - ((margin.left / 2) + 2))
-        .attr("x", 0 - (height / 2))
-        .attr("text-anchor", "middle")
-        .attr("font-size", "16px")
-        .attr("fill", "black")
-        .style("font-weight", "bold")
-        .attr("transform", "rotate(-90)")
-        .text("Smokers (%)");
-        })
-
-.catch(function(error) {
-  console.log(error)}
-  );
+// _______________________________________________________________AXIS TITLES_______________________________________________________________________//
+chartGroup.append("text").attr("transform", `translate(${width / 2}, ${height + margin.top + 13})`).style("font-weight", "bold").text("INCOME");
+chartGroup.append("text").attr("y", 0 - ((margin.left / 2) + 2)).attr("x", 0 - (height / 2)).style("font-weight", "bold").attr("transform", "rotate(-90)").text("SMOKERS (%)");})
